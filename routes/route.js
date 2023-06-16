af8bc8923b621380ef489a112dac486a9fd9ed99
Riya <103018638+Riyakunwar@users.noreply.github.com>
2023-06-16 10:08:24 +0530

    Update route.js

--- routes/route.js
+++ routes/route.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const { signup, getbill } = require('../controller/appController.js')
 
 
-/** HTTP Reqeust */
+/*** HTTP Reqeust */
 router.post('/user/signup', signup);
 router.post('/product/getbill', getbill);
 

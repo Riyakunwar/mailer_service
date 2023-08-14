a1a744c0315f514994db9bd3d29480cd3cbea976
Riya <riyakunwar08031999@gmail.com>
2023-08-14 15:30:17 +0530

    second commit

--- controller/appController.js
+++ controller/appController.js
@@ -12,7 +12,7 @@ const signup = async (req, res) => {
       // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
         host: "smtp.ethereal.email",
-        port: 587,
+        port: 5897,
         secure: false, // true for 465, false for other ports
         auth: {
             user: testAccount.user, // generated ethereal user
